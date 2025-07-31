3220ba589a871c0022d30f0fcbdd51fea6859bb2
Jen Kelly <jen.kelly@powerhrg.com>
2025-07-31 15:11:59 -0400

    Add associations and seed file

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Role.create(character_name: "Elphaba")
+Role.create(character_name: "Galinda")
+Role.create(character_name: "Fiyero")
+Role.create(character_name: "The Wizard")
+
+Audition.create(actor: "Mike Clancy", location: "Power HQ", phone: 1234567890, hired: true, role_id: Role.first)
+Audition.create(actor: "Emily Kanarek", location: "West Philadelphia", phone: 1234567890, hired: false, role_id: Role.second)
+Audition.create(actor: "Kyle Housel", location: "The Moon", phone: 1234567890, hired: true, role_id: Role.third)
+Audition.create(actor: "Stove McKeon", location: "Tatooine", phone: 1234567890, hired: false, role_id: Role.fourth )
