5c11cbf9519dd5d9d7729bb142417161a6361f4b
Jen Kelly <jen.kelly@powerhrg.com>
2025-07-31 15:29:21 -0400

    Write role method

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,11 @@
 class Audition < ActiveRecord::Base
   belongs_to :role
+
+     def role
+       Role.find(role_id)
+     end
+
+    # Audition#call_back will change the the hired attribute to true
+
+
 end
