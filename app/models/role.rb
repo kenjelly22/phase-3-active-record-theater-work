e686125745b1e787bbb6773307e397e6fc312f44
Jen Kelly <jen.kelly@powerhrg.com>
2025-07-31 16:11:51 -0400

    Write role_auditions method

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,27 @@
 class Role < ActiveRecord::Base
   has_many :auditions
+
+    def role_auditions
+      Audition.where(role_id: id)
+    end
+
+    # Role#actors returns an array of names from the actors associated with this role
+    
+
+
+    
+    # Role#locations returns an array of locations from the auditions associated with this role
+    
+
+
+    
+    # Role#lead returns the first instance of the audition that was hired for this role or returns a string 'no actor has been hired for this role'
+    
+    
+
+    
+    # Role#understudy returns the second instance of the audition that was hired for this role or returns a string 'no actor has been hired for understudy for this role'
+
+
+
 end
